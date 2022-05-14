aca8c61d779f8a29e734b6213a25df7685c1b5ae
Maurice Robert <mcosterobert@gmail.com>
2022-05-14 15:13:20 -0500

    Generate Note model, implement migration, and include Ruby Singleton module on Repo
    Add #method_missing for parity with User repo
    Correct spelling in qualifying comment on Note migration
    
    Update primary key column def in migration

---
+++ lib/scribble/repositories/note_repository.rb
@@ -0,0 +1,11 @@
+class NoteRepository < Hanami::Repository
+  include Singleton
+
+  def self.method_missing(method, *args)
+    if instance.respond_to?(method)
+      instance.send(method, *args)
+    else
+      super
+    end
+  end
+end
