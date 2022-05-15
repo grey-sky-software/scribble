433a05970531b36ff8862c1e6e8c53ed10a745af
Parker Sprouse <parker.sprouse@buspatrol.com>
2022-05-15 16:34:14 -0500

    Small tweak to entity

--- lib/notary/entities/user.rb
+++ lib/notary/entities/user.rb
@@ -1,2 +1,13 @@
 class User < Hanami::Entity
+
+  # Allows us to call all of the repository methods directly on the entity for convenience,
+  # such as `User.first` or `User.create`
+  def self.method_missing(method, *args)
+    if UserRepository.instance.respond_to?(method)
+      UserRepository.instance.send(method, *args)
+    else
+      super
+    end
+  end
+
 end
