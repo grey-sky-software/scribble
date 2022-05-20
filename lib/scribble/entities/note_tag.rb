533dbd37b8e8c8f9f7feea4d6359cb0eb34b78e2
Parker Sprouse <parker@parkersprouse.me>
2022-05-19 19:02:32 -0500

    Removed old notary references

--- lib/scribble/entities/note_tag.rb
+++ lib/scribble/entities/note_tag.rb
@@ -1,4 +1,4 @@
-require './lib/notary/repositories/note_tag_repository'
+require './lib/scribble/repositories/note_tag_repository'
 
 class NoteTag < Hanami::Entity
   # Allows us to call all of the repository methods directly on the entity for convenience,
