643b4cad7190239aa06e32daa95d66873eb11cd4
Parker Sprouse <parker.sprouse@buspatrol.com>
2022-05-25 23:29:59 -0500

    Added exposure for authenticated flag on index controller

--- apps/web/controllers/dashboard/index.rb
+++ apps/web/controllers/dashboard/index.rb
@@ -4,7 +4,15 @@ module Web
       class Index
         include Web::Action
 
+        # @return [Boolean]
+        #   whether or not the user is authenticated
+        #   used to know whether we should save the note locally or not
+        expose :authenticated
+
         def call(params)
+          # @TODO
+          #   set this value using the current authentication status
+          @authenticated = false
         end
       end
     end
