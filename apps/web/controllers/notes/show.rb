4fc8d260849c931a8f28c30a239deea5d82e98ac
Parker Sprouse <parker.sprouse@buspatrol.com>
2022-07-11 00:26:30 -0500

    Added quick note about 'halt'

--- apps/web/controllers/notes/show.rb
+++ apps/web/controllers/notes/show.rb
@@ -17,10 +17,11 @@ module Web::Controllers::Notes
     expose :note
 
     def call(_)
-      # For some reason, `halt` does not work in methods not called inside of this
-      # `#call` method.
-      # So putting the following in a `#note` method and letting the expose read
-      # that method directly, bypassing `#call` entirely, would not treat `halt` correctly.
+      # @NOTE
+      #   From what I've learned in my experimenting and research, `halt` only works when
+      #   control can be given back to the framework, and `expose` does not do that.
+      #   So if we plan on `halt`ing, we have to do it directly in the `#call` method, or
+      #   in a method whose triggering originates from the `#call` method.
       halt 404, "No Note exists with ID \"#{note_id}\"" unless Note.exist?(id: note_id)
       @note = Note.find(note_id)
     end
--- spec/web/controllers/notes/show_spec.rb
+++ spec/web/controllers/notes/show_spec.rb
@@ -2,11 +2,8 @@ RSpec.describe Web::Controllers::Notes::Show, type: :action do
   let(:action) { described_class.new }
   let(:params) { Hash[] }
 
-  # , pending: 'auth needs to be implemented'
-  it 'is successful' do
+  it 'is successful', pending: 'auth needs to be implemented' do
     response = action.call(params)
-    puts User.count
-    puts response[2]
     expect(response[0]).to eq 200
   end
 end
